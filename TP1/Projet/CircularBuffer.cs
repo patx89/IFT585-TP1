cdb17a746370121fd105635d8cad7d7f701afaf4
dragon0ne <dragonone@hotmail.fr>
2014-02-12 18:35:47 -0500

    Création de la structure de base (non définitive)
    Signed-off-by: dragon0ne

---
+++ TP1/Projet/CircularBuffer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TP1
+{
+    public class CircularBuffer
+    {
+
+        public event FreedSlotEventHandler FreedSlot;
+
+        public delegate void FreedSlotEventHandler(object sender, EventArgs e);
+
+
+        public List<Frame> _frames
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+    }
+
+    class ChangedEventHandler
+    {
+    }
+}
---
+++ TP1/Projet/Station.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TP1
+{
+    public class Station
+    {
+        public Network _network
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public CircularBuffer _buffer
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
